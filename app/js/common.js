ca368babb6cdc162de249b58640ff14477e3d17c
Viktoria <zaharova181114@gmail.com>
2021-09-03 11:43:55 +0300

    modal

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,24 @@
 $('[name="phone"]').mask('+7(999) 999-99-99');
 
 new WOW().init();
+
+//плавный скролл
+$(document).ready(function () {
+    $('.go_to').click(function (e) {
+        e.preventDefault();
+        var scroll_el = $(this).attr('href');
+        if ($(scroll_el).length !== 0) {
+            $('html, body').animate({
+                scrollTop: $(scroll_el).offset().top
+            }, 500);
+        }
+        return false;
+    });
+});
+//плавный скролл end
+
+// mobile menu
+$('.btn-burger').on('click', function () {
+   $(this).toggleClass('click');
+   $('.mobile-menu').fadeToggle();
+});
